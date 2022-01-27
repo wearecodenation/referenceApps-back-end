7f8f0951a5e4adc3a71665ab245bc37afe41dde4
Andrew Bolton <andrewbolton@Andrews-MacBook-Pro.local>
2022-01-27 18:21:06 +0000

    activity complete

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = (yargsObj) => {
       const movie = new Movie(yargsObj.title, yargsObj.actor);
       movie.add();
       console.log(movie.list());
+    } else if (yargsObj.addMulti) {
+      const movie1 = new Movie(yargsObj.title1, yargsObj.actor1);
+      const movie2 = new Movie(yargsObj.title2, yargsObj.actor2);
+      movie1.add();
+      movie2.add();
+      console.log(movie1.list());
     } else {
       console.log("Incorrect command");
     }
