2f9a84a1c4ca6bf6b0d8667746e1e7cd0aae9360
Andrew Bolton <andrewbolton@Andrews-MacBook-Pro.local>
2022-01-28 03:21:58 +0000

    activity complete

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = async (yargsObj) => {
       const movie = new Movie(yargsObj.title, yargsObj.actor);
       await movie.add();
       console.log(await movie.list());
+    } else if (yargsObj.update) {
+      const updateMovie = new Movie(yargsObj.title, yargsObj.actor);
+      await updateMovie.update(yargsObj.key, yargsObj.filter);
+    } else if (yargsObj.delete) {
+      const deleteMovie = new Movie(yargsObj.title, yargsObj.actor);
+      await deleteMovie.delete();
     } else {
       console.log("Incorrect command");
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -6,13 +6,16 @@ class Movie {
     this.title = title;
     this.actor = actor;
   }
-  async add() {
+  async grabMovies() {
     try {
       const addArr = fs.readFileSync("./storage.json");
-      movieArr = await JSON.parse(addArr);
+      movieArr = await JSON.parse(fs.readFileSync("./storage.json"));
     } catch (error) {
       movieArr = [];
     }
+  }
+  async add() {
+    await this.grabMovies();
     movieArr.push(this);
     const strArr = JSON.stringify(movieArr);
     fs.writeFileSync("./storage.json", strArr);
@@ -20,6 +23,31 @@ class Movie {
   async list() {
     movieArr = await JSON.parse(fs.readFileSync("./storage.json"));
     return movieArr;
+  }
+  async update(key, filter) {
+    await this.grabMovies();
+    for (let i = 0; i < movieArr.length; i++) {
+      if (movieArr[i][key] === filter) {
+        movieArr[i] = this;
+        break;
+      }
+    }
+    const strArr = JSON.stringify(movieArr);
+    fs.writeFileSync("./storage.json", strArr);
+  }
+  async delete() {
+    await this.grabMovies();
+    for (let i = 0; i < movieArr.length; i++) {
+      if (
+        movieArr[i].title === this.title &&
+        movieArr[i].actor === this.actor
+      ) {
+        movieArr.splice(i, 1);
+        break;
+      }
+    }
+    const strArr = JSON.stringify(movieArr);
+    fs.writeFileSync("./storage.json", strArr);
   }
 }
 
