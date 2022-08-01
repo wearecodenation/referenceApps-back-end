5e21bb5d21fe43ed491513a49767f31968598e45
Alex Arditti <68433554+Arditti93@users.noreply.github.com>
2022-08-01 10:52:24 +0100

    added comment

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5001;
 
-app.use("/static", express.static("public"));
+app.use("/static", express.static("public")); 
+// Add public folder to web server, access through /static
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
