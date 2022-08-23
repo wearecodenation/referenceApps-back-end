2a61b9c315a3ab222a162777e9e71849d6b1ee60
Arditti93 <alex.arditti@hotmail.com>
2022-08-23 11:58:56 +0100

    added notes

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,12 +5,23 @@ const client = new MongoClient(process.env.MONGO_URI);
 
 const connection = async () => {
   try {
+    //the client gibves 
     await client.connect();
+    //declair the database inside my cluster
+    //cluster for mongodb that contains db
+
+    //what we want to call the db
     const db = client.db("Movies");
-    return db.collection("Movie");
+    return db.collection("Movie"); 
+    //creates the indivual collections inside our database
+    //get the collection we use the return to perform CRUD operations on our database
   } catch (error) {
     console.log(error);
   }
 };
 
+//returning two different items so we use object syntax ;
 module.exports = { client, connection };
+
+//any file we can access the client and connection function
+//learning curve has increased since yesterday
