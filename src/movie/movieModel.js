7837b6d796682233c696d5657a98ff7c6d473772
Arditti93 <alex.arditti@hotmail.com>
2022-08-24 08:49:57 +0100

    added schema notes

--- src/movie/movieModel.js
+++ src/movie/movieModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
-
+//organization or structure for a database 
+//Essentially, it is a library that allows you to pass documents to a DBMS
+//Mongoose is an Object Document Mapping
+//defining the structure of your documents and the types of data you're storing
 const movieSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -12,6 +15,10 @@ const movieSchema = new mongoose.Schema({
   },
 });
 
-const Movie = mongoose.model("Movie", movieSchema);
+const Movie = mongoose.model("Movie2", movieSchema);
 
-module.exports = Movie;
+module.exports = Movie; 
+
+//Everything in Mongoose starts with a Schema. 
+//Each schema maps to a MongoDB collection and 
+//defines the shape of the documents within that collection.
