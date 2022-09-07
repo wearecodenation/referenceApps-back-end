8045f0d057a661f0530b65bb7a920fda53e7a800
Arditti93 <alex.arditti@hotmail.com>
2022-09-07 16:43:32 +0100

    read changes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ const { addMovie, listMovies } = require("./movie/movieFunctions");
 const app = async (yargsObj) => {
   try {
     await sequelize.sync();
-    if (yargsObj.add) {
+    if (yargsObj.create) {
       await addMovie({ title: yargsObj.title, actor: yargsObj.actor });
+      console.log("Before list")
       console.log(await listMovies());
-    } else if (yargsObj.list) {
-      console.log(await listMovies({ [yargsObj.key]: yargsObj.value }));
+    } else if (yargsObj.read) {
+      console.log(await listMovies({ [yargsObj.column]: yargsObj.title }));
     } else {
       console.log("Incorrect command");
     }
