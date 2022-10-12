f6c05721088d9adadb6a34199576a6ac63348096
Alex Arditti <68433554+Arditti93@users.noreply.github.com>
2022-10-12 09:36:23 +0100

    Added connection notes

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+//add database name between / and ? in .env
 const connection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
