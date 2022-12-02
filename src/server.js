86920f204d62262accb614a02845af81e8e20077
Alex Arditti <68433554+Arditti93@users.noreply.github.com>
2022-12-02 09:17:59 +0000

    added more beef to server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
 const express = require("express");
-const app = express();
-const port = 5001;
+const app =  express()
+const port = process.env.PORT || 5001;
 
 app.use("/static", express.static("public"));
+//access public folder when a req is made to static
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.get('/health',(req,res) => {
+	try {
+    	res.json({message:"server is ready at 5001 port "})
+    } catch (error) {
+    	consolelog(error)
+      	res.sendStatus(500)
+    }
+})
+
+app.get('/helloworld', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/html', (req, res) => {
+    res.send('<h1>Some HTML</h1>');
 });
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+});
+
+app.listen(port, ()=> {
+    console.log(`Listing on port ${port}`)
+})
